d57ff8c8032a1344163d70ea5ad75bb66f6104f9
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-06-02 19:31:00 -0400

    Linting.

--- src/RawHandler/utils.py
+++ src/RawHandler/utils.py
@@ -1,6 +1,7 @@
 import requests
 import numpy as np
 
+
 def download_file_requests(url, local_filename):
     """
     Downloads a file from a given URL using the requests library.
@@ -21,19 +22,20 @@ def download_file_requests(url, local_filename):
 
 
 def get_loss(bayer1, bayer2):
-    return ((bayer1 - bayer2)**2).mean()
+    return ((bayer1 - bayer2) ** 2).mean()
+
 
 def check_if_crop_is_valid(shape, crop_edges):
     if (crop_edges[:2] < 0).any() or (crop_edges[:2] > shape[0]).any():
-            return False
+        return False
     if (crop_edges[-2:] < 0).any() or (crop_edges[-2:] > shape[1]).any():
-            return False
+        return False
     return True
 
-def align_images(rh1, rh2, dims, 
-                 offset=(0, 0, 0, 0), max_iters=100,
-                 step_sizes = [16, 8, 4, 2]):
-    
+
+def align_images(
+    rh1, rh2, dims, offset=(0, 0, 0, 0), max_iters=100, step_sizes=[16, 8, 4, 2]
+):
     offset = np.array(offset)
     bayer1 = rh1.input_handler(dims=dims)
     img_shape = rh1.raw.shape[-2:]
@@ -42,16 +44,19 @@ def align_images(rh1, rh2, dims,
     starting_loss = loss
 
     for step_size in step_sizes:
-        directions = [np.array((step_size, step_size, 0, 0)),
-                      np.array((-step_size, -step_size, 0, 0)),
-                      np.array((0, 0, step_size, step_size)),
-                      np.array((0, 0, -step_size, -step_size))]
+        directions = [
+            np.array((step_size, step_size, 0, 0)),
+            np.array((-step_size, -step_size, 0, 0)),
+            np.array((0, 0, step_size, step_size)),
+            np.array((0, 0, -step_size, -step_size)),
+        ]
         for _ in range(max_iters):
             starting_offset = offset.copy()
             for step_dir in directions:
                 crop_edges = dims + offset + step_dir
                 # Do not update if step would create an invalid crop
-                if not check_if_crop_is_valid(img_shape, crop_edges): continue
+                if not check_if_crop_is_valid(img_shape, crop_edges):
+                    continue
                 temp_loss = get_loss(bayer1, rh2.input_handler(dims=crop_edges))
                 if temp_loss < loss:
                     offset += step_dir
